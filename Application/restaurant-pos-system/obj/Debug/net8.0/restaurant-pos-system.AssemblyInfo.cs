d7d6dbe17e4cb19a06743589d558d5c64e62b69d
Gregory-Luyckfasseel <ST10441344@vcconnect.edu.za>
2026-02-26 14:50:51 +0200

    dark modes
    added dark mode for current views and added logo png to views file

--- Application/restaurant-pos-system/obj/Debug/net8.0/restaurant-pos-system.AssemblyInfo.cs
+++ Application/restaurant-pos-system/obj/Debug/net8.0/restaurant-pos-system.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("restaurant-pos-system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e492f137c641bd5a613157e3022fffd987459f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de48ab1d04a0a81eb8ba2ecbac7713d1e5e44407")]
 [assembly: System.Reflection.AssemblyProductAttribute("restaurant-pos-system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("restaurant-pos-system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
