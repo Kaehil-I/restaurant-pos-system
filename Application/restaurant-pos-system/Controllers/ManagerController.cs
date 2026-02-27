57e17765ba6f22386a8c58cff2de8484625561c4
Kaehil-I <ST10438880@vcconnect.edu.za>
2026-02-27 08:22:04 +0200

    Manager Wireframes
    Added Zarios manager ui wireframes with empty controller.

---
+++ Application/restaurant-pos-system/Controllers/ManagerController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace restaurant_pos_system.Controllers
+{
+    public class ManagerController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult CreatedOrders()
+        {
+            return View();
+        }
+
+        public IActionResult Stock()
+        {
+            return View();
+        }
+
+        public IActionResult Staff()
+        {
+            return View();
+        }
+    }
+}
