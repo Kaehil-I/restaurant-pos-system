c3c4ee0f416fac89d768f6931ddfd387de485c9d
Kaehil-I <ST10438880@vcconnect.edu.za>
2026-03-04 09:29:25 +0200

    Added basic waitron screens
    Added MyTables screen and CreateOrder screen for Waitron users. No functionality or styling currently.

---
+++ Application/restaurant-pos-system/Controllers/WaitronController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace restaurant_pos_system.Controllers
+{
+    public class WaitronController : Controller
+    {
+        // GET: /Waitron/MyTables
+        public IActionResult MyTables()
+        {
+            return View();
+        }
+
+        // GET: /Waitron/CreateOrder
+        public IActionResult CreateOrder(int tableId)
+        {
+            ViewBag.TableId = tableId;
+            return View();
+        }
+    }   
+}
